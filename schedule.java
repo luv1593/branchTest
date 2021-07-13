9f385669290600485aa1950447e89642f06baac8
luv1593 <85241350+luv1593@users.noreply.github.com>
2021-07-13 11:50:17 -0400

    Update schedule.java

--- schedule.java
+++ schedule.java
@@ -4,6 +4,6 @@ public class schedule {
 public static void main(String[] args) {
     SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     Date date = new Date();
-    System.out.println(formatter.format(date));
+    System.out.println(formatter.format(date)); 
 }
 }
